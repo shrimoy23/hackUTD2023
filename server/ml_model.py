ac07a754ed984c801f0b44b48ccee997ef1dea0e
VishnuNambiar1234 <vishnuinsgarud007@gmail.com>
2023-11-05 09:30:32 -0600

    cool model type beat

--- server/ml_model.py
+++ server/ml_model.py
@@ -1,90 +1,73 @@
 from google.cloud import storage
 import tensorflow as tf
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Dense, Concatenate
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers, models
 import os
 
+# Set up the Google Cloud credentials and client
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/vishn/Desktop/FlaskGoogleBackend/glossy-fastness-404223-4f353d9f4ab3.json'
-
-# Create a Cloud Storage client.
 gcs = storage.Client()
 
-# Get the bucket that the file will be uploaded to.
-bucket = gcs.get_bucket('important_data_renovision')
+# Assuming you have already uploaded the processed dataset to GCS
+bucket_name = 'important_data_renovision'
+blob_name = 'processed_property_conditions.csv'
+local_file_path = 'new_processed_property_conditions.csv'
 
-# Get the blob that you want to download.
-blob = storage.Blob('processed_property_conditions.csv', bucket)
+bucket = gcs.get_bucket(bucket_name)
+blob = bucket.blob(blob_name)
+blob.download_to_filename(local_file_path)
 
-# Download the file to a destination
-blob.download_to_filename('new_processed_property_conditions.csv')
+# Load the processed CSV data into a TensorFlow Dataset
+dataset = tf.data.experimental.make_csv_dataset(
+    local_file_path,
+    batch_size=32,
+    label_name='RATING',
+    na_value="?",
+    num_epochs=1,
+    ignore_errors=True
+)
 
-# Now the file has been downloaded locally, you can use a local file path
-csv_file_path = 'new_processed_property_conditions.csv'
+# Rename the column
+dataset = dataset.map(lambda x, y: ({'roof_type_solar_panel' if k == 'roof_type_solar panel' else k: v for k, v in x.items()}, y))
 
-# Load the CSV data into a TensorFlow Dataset
-def get_dataset(file_path, label_name, batch_size=32):
-    dataset = tf.data.experimental.make_csv_dataset(
-        file_path,
-        batch_size=batch_size,
-        label_name=label_name,
-        na_value="?",
-        num_epochs=1,
-        ignore_errors=True,
-        header=True
-    )
-    return dataset
+# Define the feature columns
+feature_columns = []
+for header in ['holes_cracks', 'cosmetic_dmg', 'window_count', 'latitude', 'longitude']:
+    feature_columns.append(tf.feature_column.numeric_column(header))
 
-# Use the utility function to create the dataset
-train_dataset = get_dataset(csv_file_path, label_name='RATING')
+# The categorical features have been one-hot encoded, so they're already represented as numeric columns
+for header in ['property_type', 'trending_style', 'wall_type_metal', 'wall_type_wood',
+               'roof_type_solar_panel', 'roof_type_tile', 'lawn_type_none', 'lawn_type_small',
+               'interior_quality_fair', 'interior_quality_good', 'interior_quality_poor']:
+    feature_columns.append(tf.feature_column.numeric_column(header))
 
-# Define which features are categorical
-categorical_features = [
-    'wall_type', 'roof_type', 'lawn_type', 'interior_quality', 'trending_style'
-]
+# Create a feature layer from the feature columns
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
 
-# Define which features are numerical
-numerical_features = [
-    'holes_cracks', 'cosmetic_dmg_level', 'window_count'
-]
+# Build, compile, and train the model
+model = models.Sequential([
+    feature_layer,
+    layers.Dense(128, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1)
+])
 
-# Define the input layers for each feature
-inputs = {
-    feature: Input(name=feature, shape=(), dtype='string') 
-    if feature in categorical_features else Input(name=feature, shape=(), dtype='float32') 
-    for feature in categorical_features + numerical_features
-}
-# Preprocess the input layers if necessary (e.g., normalization, encoding)
-# You need to define these preprocessing steps based on your feature types and requirements
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['mean_absolute_error'])
 
-# Combine all inputs into a single list (preserving the order of feature_names)
-combined_inputs = Concatenate()(list(inputs.values()))
-
-# Define the rest of the neural network
-x = Dense(64, activation='relu')(combined_inputs)
-x = Dense(64, activation='relu')(x)
-output = Dense(1)(x)
-
-# Create the model
-model = Model(inputs=inputs, outputs=output)
-
-# Compile the model with the Adam optimizer and mean squared error loss function
-model.compile(optimizer=Adam(), loss='mean_squared_error')
-
-# Train the model
 # Convert the dataset into a format that contains a dictionary with feature names mapping to tensor values
 def preprocess(features, label):
-    return {key: value for key, value in features.items()}, label
+    return features, label
 
-train_dataset = train_dataset.map(preprocess)
+train_dataset = dataset.map(preprocess)
 
-# Train the model with the dataset
-model.fit(train_dataset, epochs=10, steps_per_epoch=30)
-
-# Save the trained model to Google Cloud Storage
-# Ensure that the Google Cloud environment is properly configured to save directly to GCS
-model_save_path = 'saved_model' # Temporarily save locally
+# Train the model
+model.fit(train_dataset, epochs=1000)
+loss, mae = model.evaluate(train_dataset)
+print(f"Model Mean Absolute Error: {mae}")
+# Save the trained model
+model_save_path = 'saved_model_new'
 model.save(model_save_path)
 
-# Upload the saved model to GCS
-bucket.blob('saved_model').upload_from_filename(model_save_path)
+# Print a summary of the model
+model.summary()
